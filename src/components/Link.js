c8fd186965b985407558c3165092d15df40f04e5
Tim Kendall <tim@acorns.com>
2017-01-22 20:03:00 -0800

    Add simplistic logic for styling an active Link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
 
-const Link = ({view, className, params = {}, queryParams = {}, store = {}, refresh = false, style = {}, children, title = children, router = store.router}) => {
+const Link = ({view, className, params = {}, queryParams = {}, store = {}, refresh = false, style = {}, activeStyle = {}, activeClassName = '', children, title = children, router = store.router}) => {
   if (!router) {
     return console.error('The router prop must be defined for a Link component to work!')
   }
+
+  // TODO - Do an actual comparison here (dont think object refs will be the same?)
+  const isCurrentView = router.currentView === view 
+    && router.params === params 
+    && router.queryParams === queryParams;
+  const currentStyle = isCurrentView ? { ...style, ...activeStyle } : style;
+  const currentClassName = isCurrentView ? `${className} ${activeClassName}` : className;
+
   return (<a
-      style={style}
-      className={className}
+      style={currentStyle}
+      className={currentClassName}
       onClick={e => {
         const middleClick = e.which == 2;
         const cmdOrCtrl = (e.metaKey || e.ctrlKey);
